d4bec17fb4210d24064905a1dc90fafdeec30dd7
karlasunjara <ksunjara@calpoly.edu>
2019-04-09 15:11:19 -0700

    Moved file to new directory

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+
+class Car {
+	String make;
+	String model;
+	int year;
+	String color;
+	int licensePlate;
+	
+	public Car(String make, String model, int year, String color, int licensePlate) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.color = color;
+		this.licensePlate = licensePlate;
+	}
+}
