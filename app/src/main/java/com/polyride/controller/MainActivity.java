e623fcd3dd8c8fd1065a2ede5f0d86bc81bf418e
nguyen-darianhuy <dariann6@gmail.com>
2019-05-04 19:48:58 -0700

    Add Main and Request pages
    Adds Karla's pages.

---
+++ app/src/main/java/com/polyride/controller/MainActivity.java
@@ -0,0 +1,33 @@
+package com.polyride.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+import com.polyride.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, RequestActivity.class);
+        startActivity(intent);
+    }
+}
