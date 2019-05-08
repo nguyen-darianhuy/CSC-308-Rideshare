87924ff0c2d20562f1fff1a642f3310286688ea2
ksunjara <ksunny129@yahoo.com>
2019-05-07 22:11:02 -0700

    Karla's change

--- app/src/main/java/com/polyride/controller/RegisterActivity.java
+++ app/src/main/java/com/polyride/controller/RegisterActivity.java
@@ -1,16 +1,21 @@
 package com.polyride.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.polyride.R;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private Button button;
+    private Button nextbutton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +44,32 @@ public class RegisterActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        button = (Button)findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity3();
+            }
+        });
+
+        nextbutton = (Button)findViewById(R.id.button7);
+        nextbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity4();
+            }
+        });
     }
+
+    public void openActivity3(){
+        Intent intent = new Intent(this, RidesActivity.class);
+        startActivity(intent);
+    }
+    public void openActivity4(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 
 }
