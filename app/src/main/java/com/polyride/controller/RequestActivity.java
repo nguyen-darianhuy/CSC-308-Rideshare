8e55378e8420482cf3b2abd8e5e30101be78056d
turrence <tjtong2006@gmail.com>
2019-05-28 11:41:45 -0700

    removed unused dependencies

--- app/src/main/java/com/polyride/controller/RequestActivity.java
+++ app/src/main/java/com/polyride/controller/RequestActivity.java
@@ -10,7 +10,7 @@ public class RequestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_create);
     }
 
 
