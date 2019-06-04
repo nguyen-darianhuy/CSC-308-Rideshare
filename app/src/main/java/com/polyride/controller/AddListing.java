85288475871c7e16b71f376c800dc790045e756a
nguyen-darianhuy <dariann6@gmail.com>
2019-06-03 22:11:03 -0700

    Add support for creating listing

--- app/src/main/java/com/polyride/controller/AddListing.java
+++ app/src/main/java/com/polyride/controller/AddListing.java
@@ -1,8 +1,11 @@
 package com.polyride.controller;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,23 +13,35 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.polyride.R;
-
-import org.w3c.dom.Text;
+import com.polyride.entity.TripListing;
+import com.polyride.entity.User;
 
 import java.util.Calendar;
-
-import io.opencensus.tags.Tag;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddListing extends AppCompatActivity { //NOSONAR
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
 
     private static final String TAG = "AddListing";
 
     private TextView mDisplayDate;
-    private TextView mDisplayDate2;
 
     private DatePickerDialog.OnDateSetListener mDateSetListener;
-    private DatePickerDialog.OnDateSetListener mDateSetListener2;
+    private TextInputLayout departureLayout;
+    private TextInputLayout destinationLayout;
+    private TextInputLayout maxPassengersLayout;
 
 
     @Override
@@ -34,7 +49,6 @@ public class AddListing extends AppCompatActivity { //NOSONAR
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_listing);
         mDisplayDate = (TextView) findViewById(R.id.DatePicker);
-        mDisplayDate2 = (TextView) findViewById(R.id.DatePicker1);
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,33 +78,56 @@ public class AddListing extends AppCompatActivity { //NOSONAR
             }
         };
 
-        mDisplayDate2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
+        departureLayout = findViewById(R.id.textInputLayout);
+        destinationLayout = findViewById(R.id.textInputLayout2);
+        maxPassengersLayout = findViewById(R.id.textInputLayout4);
 
-                DatePickerDialog dialog = new DatePickerDialog(
-                        AddListing.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener2,
-                        year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
 
-            }
-        });
-        mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month = month + 1;
-                Log.d(TAG, "onDateSet: mm/dd/yyy:" + month + "/" + dayOfMonth + "/" + year);
-                String date = month + "/" + dayOfMonth + "/" + year;
-                mDisplayDate2.setText(date);
-            }
-        };
+        if (user != null) {
+
+        }
 
     }
+
+    public void submit(View view) { // NO SONAR
+        final DocumentReference docRef = db.collection("Users").document(user.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    User driver = new User();
+                    driver.setUserAttributes(document);
+                    createListing(driver);
+                }
+            }
+        });
+    }
+
+    public void createListing(final User driver) {
+        Map<String, Object> listingData = new HashMap<>();
+        listingData.put("departure", departureLayout.getEditText().getText().toString());
+        listingData.put("departureDate", mDisplayDate.getText().toString());
+        listingData.put("destination", destinationLayout.getEditText().getText().toString());
+        listingData.put("driverID", user.getUid());
+        listingData.put("maxPassengers", Integer.parseInt(maxPassengersLayout.getEditText().getText().toString()));
+        listingData.put("numPassengers", 0);
+
+        final DocumentReference docRef = db.collection("TripListing").document();
+        docRef.set(listingData).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                openRideListing();
+            }
+        });
+    }
+
+    public void openRideListing(){
+        Intent intent = new Intent(this, RidesActivity.class);
+        startActivity(intent);
+    }
+
 }
