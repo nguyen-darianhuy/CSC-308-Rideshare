c00b12368d8e7784edab305c041dfe66e19a45ac
nguyen-darianhuy <dariann6@gmail.com>
2019-06-05 14:01:32 -0700

    Fix smells in Profile page

--- app/src/main/java/com/polyride/controller/Profile2.java
+++ app/src/main/java/com/polyride/controller/Profile2.java
@@ -1,6 +1,7 @@
 package com.polyride.controller;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TextInputLayout;
@@ -37,7 +38,7 @@ public class Profile2 extends AppCompatActivity { //NOSONAR
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile2);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.nav_view);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -48,6 +49,12 @@ public class Profile2 extends AppCompatActivity { //NOSONAR
 
                     case R.id.navigation_notifications:
                         openNotifications();
+                        break;
+
+                    case R.id.navigation_profile:
+                        openProfile();
+
+                    default:
                         break;
                 }
                 return true;
@@ -124,6 +131,11 @@ public class Profile2 extends AppCompatActivity { //NOSONAR
 
     public void openLogin(){
         Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void openProfile(){
+        Intent intent = new Intent(this, Profile2.class);
         startActivity(intent);
     }
 
